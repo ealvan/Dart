3f905b0c9c30a89cc5582bc886a5b71d93892bbd
EDSEL <ealvan@unsa.edu.pe>
2020-11-21 21:17:59 -0500

    creando form interface register user

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'estudio_time/time.dart';
 
+//import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 void main() {
   runApp(MyApp());
 }
@@ -17,6 +18,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ************************************************
+class EMailTextFormField extends StatefulWidget {
+  final void Function(String email) onSaved;
+
+  const EMailTextFormField({Key key, this.onSaved}) : super(key: key);
+
+  @override
+  EMailTextFormFieldState createState() => EMailTextFormFieldState();
+}
+
+class EMailTextFormFieldState extends State<EMailTextFormField> {
+  final _email = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          icon: Icon(Icons.email,
+              size: IconTheme.of(context).size,
+              color: IconTheme.of(context).color),
+          labelText: 'E-Mail...',
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
+        ),
+        keyboardType: TextInputType.emailAddress,
+        controller: _email,
+        validator: _validateEmail,
+        maxLength: 70,
+        onSaved: widget.onSaved, //callback
+      ),
+    );
+  }
+
+  String _validateEmail(String email) {
+    // validate E-Mail function...
+  }
+}
+
+//************************************************ */
 class MyHome extends StatefulWidget {
   MyHome({Key key, this.title}) : super(key: key);
   final String title;
@@ -24,29 +65,68 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHome extends State<MyHome> {
-  TextStyle style = TextStyle(fontFamily: "Montserrat", fontSize: 20.0);
-
+  TextStyle style = TextStyle(fontFamily: "Montserrat", fontSize: 4.0);
+  String _email;
   Widget build(BuildContext context) {
-    final myController_nombre = TextEditingController();
+    final myController_usuario = TextEditingController();
     final myController_apellidos = TextEditingController();
     final myController_nacimiento = TextEditingController();
     void dispose() {
       // Clean up the controller when the widget is disposed.
-      myController_nombre.dispose();
+      myController_usuario.dispose();
       myController_apellidos.dispose();
       myController_nacimiento.dispose();
       super.dispose();
     }
 
-    final nombre = TextField(
-        controller: myController_nombre,
-        style: style,
+    // User field
+    final usuario = TextField(
+        controller: myController_usuario,
         decoration: InputDecoration(
+          icon: Icon(Icons.person,
+              size: IconTheme.of(context).size,
+              color: IconTheme.of(context).color),
           contentPadding: EdgeInsets.all(5.0),
-          hintText: "Nombres",
+          hintText: "ingrese su usuario",
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
         ));
-
+    // email Field
+    final emailField = EMailTextFormField(
+      onSaved: (String email) => _email = email,
+    );
+    final googleButton = TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.email,
+            size: IconTheme.of(context).size,
+            color: IconTheme.of(context).color),
+        labelText: 'Ingrese su email',
+      ),
+    );
+    //contrasenia
+    final password = TextField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.lock_open_rounded,
+            size: IconTheme.of(context).size,
+            color: IconTheme.of(context).color),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
+        hintText: 'ingrese su contraseña',
+      ),
+      autofocus: false,
+      obscureText: true,
+    );
+    //CON firm Password
+    final confirmPassword = TextField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.lock_open_rounded,
+            size: IconTheme.of(context).size,
+            color: IconTheme.of(context).color),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
+        hintText: 'confirme su contraseña',
+      ),
+      autofocus: false,
+      obscureText: true,
+    );
+//*************************************** */
     final apellidos = TextField(
         controller: myController_apellidos,
         style: style,
@@ -63,8 +143,9 @@ class _MyHome extends State<MyHome> {
           hintText: "Fecha de Nacimiento",
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
         ));
+//***************************************
 
-    final loginButon = Material(
+    final registerButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
       color: Color(0xff01A0C7),
@@ -72,21 +153,18 @@ class _MyHome extends State<MyHome> {
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.all(5.0),
         onPressed: () {
-          Cabala persona1 = new Cabala(
-              myController_nombre.text + ' ' + myController_apellidos.text,
-              myController_nacimiento.text);
           return showDialog(
             context: context,
             builder: (context) {
               return AlertDialog(
                 // Retrieve the text the user has entered by using the
                 // TextEditingController.
-                content: Text(persona1.toString()),
+                content: Text('su registro fue exitoso!!'),
               );
             },
           );
         },
-        child: Text("Enterate!",
+        child: Text("Registrate!",
             textAlign: TextAlign.center,
             style: style.copyWith(
                 color: Colors.white, fontWeight: FontWeight.bold)),
@@ -97,23 +175,27 @@ class _MyHome extends State<MyHome> {
         child: Container(
           color: Colors.white,
           child: Padding(
-            padding: const EdgeInsets.all(5.0),
+            padding: const EdgeInsets.all(1.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SizedBox(height: 5.0),
-                nombre,
+                usuario,
                 SizedBox(height: 5.0),
-                apellidos,
+                emailField,
                 SizedBox(
-                  height: 15.0,
+                  height: 5.0,
                 ),
-                fechaNacimiento,
+                password,
                 SizedBox(
-                  height: 15.0,
+                  height: 5.0,
                 ),
-                loginButon,
+                confirmPassword,
+                SizedBox(
+                  height: 5.0,
+                ),
+                registerButton,
               ],
             ),
           ),
